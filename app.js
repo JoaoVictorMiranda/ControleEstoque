83ec879358eaf2e3cbc2073916a525eb15d0d2a1
João Victor Miranda <122116902+JoaoVictorMiranda@users.noreply.github.com>
2025-06-16 15:49:54 -0300

    Menu inicial Iniciado

--- app.js
+++ app.js
@@ -4,8 +4,39 @@ import * as Function from './functions/functions.js';
 Function.Deletar();
 //FRUFRU pra fingir que o programa esta fazendo calculos para abrir
 await Function.sleep(1000);
+let verificar = true;
 
-Function.Menu();
+
+while (verificar) {
+    Function.Menu();
+    let Resp = Function.PerguntarMensagem("Qual deseja? ", "Number");
+    switch (Resp) {
+        case 1:
+            Function.ListarEstoque();
+            break;
+        case 2:
+            // Adicionar produto
+            break;
+        case 3:
+            //Remover Produto
+            break;
+        case 4:
+            //Deletar produto
+            break;
+        case 0:
+            Function.Deletar();
+            Function.sleep(500);
+            Function.Finalizar();
+            verificar = false;
+        //SAIR
+    }
+}
 
 
-let n = Function.PerguntarMensagem("Opção: ", "Number")
+/* ✅ Cadastrar produto
+✅ Listar produtos
+✅ Adicionar ao estoque
+✅ Retirar do estoque
+✅ Apagar produto
+✅ Sair do program */
+
--- functions/functions.js
+++ functions/functions.js
@@ -4,7 +4,12 @@ const input = prompt();
 export function Menu() {
     console.log(`
             ### Estoque CEBB São Paulo (Centro de Estudos Budistas Bodisatva) ###
-            O que gostaria?
+            1. Cadastrar Produto
+            2. Listar Todo o estoque
+            3. Adicionar produto ao estoque
+            4. Remover produto do estoque
+            5. Deletar produto do estoque
+            0. Sair
         `)
 }
 
@@ -26,9 +31,52 @@ export function Deletar() {
     return process.stdout.write("\x1B[2J\x1B[0f");
 }
 
+export function Finalizar() {
+    console.log(`Muito obrigado por participar volte sempre`);
+}
+
 //Sleep basico pra fazer FRUFRU de esperar até abrir.
 export async function sleep(ms) {
     return new Promise((ok) => {
         setTimeout(ok, ms);
     });
 }
+
+
+
+export function ListarEstoque() {
+    for (let item of estoque) {
+        console.log(item.nome, item.Qtd_OBJETO);
+    }
+}
+
+
+
+
+let estoque = [
+    {
+        nome: "OBJETO",
+        Qtd_OBJETO: 10,
+        Descricao: "Descrição do objeto"
+    },
+    {
+        nome: "OBJETO2",
+        Qtd_OBJETO: 10,
+        Descricao: "Descrição do objeto"
+    },
+    {
+        nome: "OBJETO3",
+        Qtd_OBJETO: 10,
+        Descricao: "Descrição do objeto"
+    },
+    {
+        nome: "OBJETO4",
+        Qtd_OBJETO: 10,
+        Descricao: "Descrição do objeto"
+    },
+    {
+        nome: "OBJETO5",
+        Qtd_OBJETO: 10,
+        Descricao: "Descrição do objeto"
+    },
+]
